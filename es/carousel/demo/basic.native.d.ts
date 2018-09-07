31a6cd38847b6a95603ce3cff653f1bd31b3f224
杨俊宁 <1003719811@qq.com>
2018-09-07 14:25:04 +0800

    删除demo

--- es/carousel/demo/basic.native.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react" />
-import React from 'react';
-export default class BasicCarouselExample extends React.Component<any, any> {
-    onHorizontalSelectedIndexChange(index: number): void;
-    onVerticalSelectedIndexChange(index: number): void;
-    render(): JSX.Element;
-}
